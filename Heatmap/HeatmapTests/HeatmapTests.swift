6f0eadad9c9426de9c09d63f385b71900d63bd19
egordmitriev-creator <edmitriev377@gmail.com>
2026-05-26 17:14:44 +0700

    hw_21

---
+++ Heatmap/HeatmapTests/HeatmapTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeatmapTests.swift
+//  HeatmapTests
+//
+//  Created by egor_dmitriev on 26.05.2026.
+//
+
+import Testing
+@testable import Heatmap
+
+struct HeatmapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
